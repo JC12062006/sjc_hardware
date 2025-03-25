e356bd5eb02857e794fa15ee5b9e64b57923b6ce
Kristof-sZn <siazon.jeanchristophe.pro@gmail.com>
2025-03-25 10:15:56 +0100

    Modif SQL

--- bdd/script.sql
+++ bdd/script.sql
@@ -4,5 +4,20 @@ USE sjc_hardware;
 
 -- Table utilisateur -- 
 
-CREATE TABLE utilisateur
+CREATE TABLE utilisateur(
 id INT AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR (255) NOT NULL,
+prenom VARCHAR (255) NOT NULL,
+email VARCHAR (255) NOT NULL UNIQUE,
+tel VARCHAR (11) NOT NULL,
+rôle BOOLEAN NOT NULL 1
+);
+
+-- Table demande --
+
+CREATE TABLE demande(
+id INT AUTO_INCREMENT PRIMARY KEY,
+titre VARCHAR (255) NOT NULL,
+description_ VARCHAR (9999)
+);
+
